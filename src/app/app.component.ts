e099d837c4715808294a44bd68550d6cfb01be90
andrupax94 <52213160+andrupax94@users.noreply.github.com>
2023-09-23 19:49:09 +0100

    sdfsdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,33 @@
 import { Component } from '@angular/core';
 import * as Phaser from 'phaser';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   config = {
     type:Phaser.AUTO,
     width:800,
     height:600,
     scene:{
-      performance:this.preload,
-      create:this.create,
-      update:this.update
+      preload: ()=>this.preload.bind(this), // Vincular manualmente el contexto
+      create: ()=>this.create.bind(this), // Vincular manualmente el contexto
+      update: ()=>this.update.bind(this) // Vincular manualmente el contexto
     }
   }
   game = new Phaser.Game(this.config);
-
-  preload(){
+  constructor(){
+  }
+  preload(t:Phaser.Scene){
+    t.load.image('back','/assets/mainBackG.jpg');
+  }
+  create(t:Phaser.Scene){
 
   }
-  create(){
+  update(t:Phaser.Scene){
 
   }
-  update(){
-
-  }
-
-  constructor(){}
 }
