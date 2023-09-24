db7180c87f56e8b1609aa235e046d292a1229ad5
andrupax94 <52213160+andrupax94@users.noreply.github.com>
2023-09-24 21:57:35 +0100

    fhghfgh

--- src/app/class/scene.ts
+++ src/app/class/scene.ts
@@ -9,6 +9,7 @@ export class Scene extends Phaser.Scene
     preload ()
     {
         this.load.image('red', 'assets/mainBackG.jpg');
+        this.load.spritesheet('pj','assets/mainBackG.jpg',{frameWidth:80,frameHeight:80});
     }
 
     create ()
